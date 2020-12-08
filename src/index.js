e3e7567d1d6ac932b5dbfe1e53207eff4080a840
Temmarie <temmar_whyte16@yahoo.com>
2020-12-08 10:29:50 +0100

    Update index.js for webpack compilation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import Phaser from '../js/phaser.min';
-import SceneMain from '../js/sceneMain';
-import StartScene from '../js/startScene';
+import Phaser from '../src/phaser.min';
+import SceneMain from '../src/js/sceneMain';
+
 
 
  const config = {
@@ -9,7 +9,6 @@ import StartScene from '../js/startScene';
     height: 640,
     parent: 'phaser-game',
     scene: [
-      StartScene,
       SceneMain]
   };
   var game = new Phaser.Game(config);
