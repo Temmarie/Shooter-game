60e94c4083f30d5d2697ad439ec48433e3607b45
Temmarie <temmar_whyte16@yahoo.com>
2020-12-08 10:40:32 +0100

    Update webpack for audio and image compilation

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.wav$/i,
+        use: 'file-loader'
+      }
     ],
   },
 };
